62ff3c4189ab2c9aa688678dbc4c43cdeb3a1b95
kenny-io <ekeneeze3@gmail.com>
2021-01-01 14:16:36 +0400

    Getting started

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Registration />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Registration from './components/Registration.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Registration
   }
 }
 </script>
